d092c9e700a2b6f23239a0a152973bf8fc63c003
IsuruSampathRanasinghe <isuruep1@gmail.com>
2025-12-17 19:12:30 +0530

    Intergrating SignUp APIs 🖋️

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/input";
 import { validateEmail } from "../../Utils/helper";
 import ProfilePhotoSelector from "../../components/Inputs/profilePhotoSelector";
+import axiosInstance from "../../Utils/axiosInstance";
+import { API_PATHS } from "../../Utils/apiPaths";
+import { UserContext } from "../../context/userContext";
+import uploadImage from "../../Utils/uploadImage";
 
 
 
@@ -15,6 +19,7 @@ const SignUp = () => {
 
     const [error, setError] = useState(null);
 
+    const {updateUser} = useContext(UserContext)
     const navigate = useNavigate();
 
     // Handle Sign Up form Submit
@@ -41,8 +46,37 @@ const SignUp = () => {
         setError("");
 
         // SignUp API Call
-        
-    }
+        try{
+
+            // Upload image if present
+            if(profilePic){
+                const imgUploadRes = await uploadImage(profilePic);
+                profileImageUrl = imgUploadRes.imageUrl || "";
+            }
+
+            const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+                fullName,
+                email,
+                password,
+                profileImageUrl
+            });
+
+            const {token, user} = response.data;
+
+            if(token){
+                localStorage.setItem("token", token);
+                updateUser(user);
+                navigate("/dashboard");
+            }
+        } catch (error){
+            if(error.response && error.response.data.message){
+                setError(error.response.data.message);
+            }else{
+                setError("Something went wrong. Please try again.");
+            }
+        }
+
+    };
 
     return (
         <AuthLayout>
