8e0766991771f964e5d02279dcc0f10582c56732
IsuruSampathRanasinghe <isuruep1@gmail.com>
2026-01-02 12:26:50 +0530

    Income List Section 🖋️

--- frontend/src/components/Income/IncomeList.jsx
+++ frontend/src/components/Income/IncomeList.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { LuDownload } from "react-icons/lu";
+import Income from "../../pages/Dashboard/Income";
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
+import moment from "moment";
 
 const IncomeList = ({transactions, onDelete, onDownload}) => {
     return(
-        <div>IncomeList</div>
+        <div className="card">
+            <div className="flex items-center justify-between">
+                <h5 className="text-lg">Income Sources</h5>
+
+                <button className="card-btn" onClick={onDownload}>
+                    <LuDownload className="text-base" /> Download
+                </button>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                {transactions?.map((Income) => (
+                    <TransactionInfoCard
+                        key={Income._id}
+                        title={Income.source}
+                        icon={Income.icon}
+                        date={moment(Income.date).format("Do MMM YYYY")}
+                        amount={Income.amount}
+                        type="income"
+                        onDelete={() => onDelete(Income._id)}
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
 
